b1a5b6604323efc0bfbe568da1cfee20309449bc
Mate Farkas <mate.farkas@sch.hu>
2020-06-15 00:19:22 +0100

    Add build plugin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,64 +1,9 @@
-import org.jetbrains.kotlin.config.KotlinCompilerVersion
-
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("kotlin-android-extensions")
-    id("kotlin-kapt")
+    id("y2k.dash.build")
 }
 
 android {
-    compileSdkVersion(29) // Q
-    defaultConfig {
-        applicationId = "y2k.dash"
-        minSdkVersion(21) // LOLLIPOP
-        targetSdkVersion(29) // Q
-        versionCode = 6
-        versionName = "1.3"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    buildTypes {
-        getByName("release") {
-             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
-}
-
-kapt {
-    arguments {
-        arg("room.schemaLocation", "$projectDir/schemas/")
-    }
-}
-
-dependencies {
-    val lifecycleVersion = "2.2.0"
-    val roomVersion = "2.2.5"
-
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
-    androidTestImplementation("androidx.test:runner:1.2.0")
-
-    api("com.google.android.material:material:1.2.0-alpha06")
-
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.fragment:fragment-ktx:1.2.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    implementation("androidx.recyclerview:recyclerview:1.1.0")
-    implementation("androidx.room:room-ktx:$roomVersion")
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-rc01")
-    implementation("com.android.volley:volley:1.1.1")
-    implementation("org.jetbrains.anko:anko-commons:0.10.5")
-
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
-    implementation(kotlin("stdlib-jdk7", KotlinCompilerVersion.VERSION))
-
-    kapt("androidx.room:room-compiler:$roomVersion")
-
-    testImplementation("junit:junit:4.12")
 }
