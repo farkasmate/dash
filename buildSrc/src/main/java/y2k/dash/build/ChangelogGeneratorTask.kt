3d00e272242b1bdb654bbfc6700e135732ec2552
Mate Farkas <mate.farkas@sch.hu>
2020-07-01 00:48:07 +0100

    Generate changelog for all versions.

--- buildSrc/src/main/java/y2k/dash/build/ChangelogGeneratorTask.kt
+++ buildSrc/src/main/java/y2k/dash/build/ChangelogGeneratorTask.kt
@@ -5,11 +5,12 @@ import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 open class ChangelogGeneratorTask : DefaultTask() {
-    private val parser = ChangelogParser(Settings.tagsDirectory, BuildFlavor.SHARED)
-
     @TaskAction
     fun generate() {
-        val appChangelogFile = File(Settings.changelogDirectory, "${parser.versionCode}.txt")
-        parser.changelogFile.copyTo(appChangelogFile, overwrite = true)
+        BuildFlavor.values().forEach { flavor ->
+            val parser = ChangelogParser(Settings.tagsDirectory, flavor)
+            val appChangelogFile = File(Settings.changelogDirectory, "${parser.versionCode}.txt")
+            parser.changelogFile.copyTo(appChangelogFile, overwrite = true)
+        }
     }
 }
